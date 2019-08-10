ed45bdc60bbab7f7391bf34f1a7351b6d7320937
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-08-10 08:45:46 -0300

    Importação do objeto HttpClient via Injeção de Dependência, que possibilita o uso direto deste e de qualquer outro objeto sem precisar saber detalhes de sua instanciação. Para tornar possível o uso deste objeto, foi necessário importar o HttpClientModule no módulo da aplicação. O HttpClient foi utilizado para integrar a SPA à API, enviando uma requisição get para pegar todas as fotos de um dos usuários registrados na API. Ao obter estas fotos, o HttpClient retorna um Observable com o conjunto de dados recuperados em formato JSON. Observable é uma das ferramentas que o Angular também utiliza para lidar com operações assíncronas. Para poder tratar o retorno da API, é necessário chamar a função subscribe e passar para ela uma função de tratamento, que no caso, é atribuir o retorno ao atributo da classe desde que eles tenham o mesmo tipo. Para tratamento de erros, é necessário passar uma segunda função de parâmetro para o subscribe, realizando os tratamentos necessários.

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photo.module';
@@ -10,7 +11,8 @@ import { PhotosModule } from './photos/photo.module';
   ],
   imports: [
     BrowserModule,
-    PhotosModule
+    PhotosModule, 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
