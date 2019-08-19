361c2c84fb4ef73262b0813dd4b4dd84b173b710
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-08-19 08:08:04 -0300

    Implementação do LoadButton, utilizando ng-template e diretiva ngIf para verificar se há mais fotos para ser exibidas ao clicar no botão e, caso não haja, exibir apenas uma notificaçãop informando que não há mais fotos.

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -5,6 +5,7 @@ import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -13,13 +14,20 @@ import { Photo } from '../photo/photo';
 })
 export class PhotoListComponent implements OnInit, OnDestroy {
   
-  photos: Photo[] = [];
   filter: string = '';
   debounce: Subject<string> = new Subject<string>();
+  photos: Photo[] = [];
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
   
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+    ) { }
   
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data.photos;
     this.debounce
     .pipe(debounceTime(300))
@@ -28,5 +36,16 @@ export class PhotoListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, this.currentPage++)
+      .subscribe(photos => {
+        this.photos.push(...photos);
+        if (!photos.length) {
+          this.hasMore = false;
+        }
+      });
   }
 }
