5536df2c1da253d053c167ce494245b4001f9098
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-08-20 06:21:25 -0300

    Criação de submódulos no código da aplicação, de forma a reduzir o número excessivo de imports e aumentar a organização. Os módulos se tornam agregadores de outros módulos que podem ser importados por outros módulos de forma independente. Cada módulo possui suas próprias dependências e importa somente o que precisa usar.

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -42,7 +42,7 @@ export class PhotoListComponent implements OnInit, OnDestroy {
     this.photoService
       .listFromUserPaginated(this.userName, this.currentPage++)
       .subscribe(photos => {
-        this.photos.push(...photos);
+        this.photos = this.photos.concat(photos);
         if (!photos.length) {
           this.hasMore = false;
         }
