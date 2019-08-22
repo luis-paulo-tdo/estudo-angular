0043f21615dd51698349de5f3dcc69b61776c6d0
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-08-22 07:18:42 -0300

    Componentização do filtro de busca, uma vez que toda sua estrutura HTML estava sendo exposta para a aplicação. A ideia do Angular é que os arquivos HTML sejam mínimos. Com isso, foi criado um SearchComponent dentro de PhotoListComponent e tanto o Debounce quanto o OnDestroy foi passado para ele. Com esta mudança, entretanto, a barra de busca deixou de funcionar, pois ela agora se tornou uma filha de PhotoListComponent em vez de parte integrante dele. É preciso criar uma estratégia para fazer o componente filho (SearchComponent) se comunicar com o componente pai (PhotoListComponent).

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,21 +1,17 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
-  selector: 'app-photo-list',
+  selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
   
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
   photos: Photo[] = [];
   hasMore: boolean = true;
   currentPage: number = 1;
@@ -29,13 +25,6 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data.photos;
-    this.debounce
-    .pipe(debounceTime(300))
-    .subscribe(filter => this.filter = filter);
-  }
-
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
   }
 
   load() {
--- alurapic/src/app/photos/photo-list/photo-list.module.ts
+++ alurapic/src/app/photos/photo-list/photo-list.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { PhotoListComponent } from './photo-list.component';
-import { PhotosComponent } from './photos/photos.component';
-import { LoadButtonComponent } from './load-button/load-button.component';
-import { FilterByDescriptionPipe } from './filter-by-description.pipe';
 import { CommonModule } from '@angular/common';
-import { PhotoModule } from '../photo/photo.module';
+import { NgModule } from '@angular/core';
+
 import { CardModule } from 'src/app/shared/components/card/card.module';
+import { FilterByDescriptionPipe } from './filter-by-description.pipe';
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { PhotoListComponent } from './photo-list.component';
+import { PhotoModule } from '../photo/photo.module';
+import { PhotosComponent } from './photos/photos.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
     declarations: [
+        FilterByDescriptionPipe,
+        LoadButtonComponent,
         PhotoListComponent,
         PhotosComponent,
-        LoadButtonComponent,
-        FilterByDescriptionPipe
+        SearchComponent
     ],
     imports: [
         CommonModule,
