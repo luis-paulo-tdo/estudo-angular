20356bc6060f88cdbdcebae5bdb5bff38c625a49
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-08-24 11:58:34 -0300

    Componentização da estrutura de mensagem de validação, que poderá ser reutilizada não só no Template do SignInComponent, mas em todo resto da aplicação. As classes do Bootstrap não precisarão ser declaradas toda vez que a tag for usada. O texto se torna uma Inbound Property do Componente, e cada um deles terá o seu ngIf definido pelo formGroup. Assim, a tag fica mais enxuta, a duplicidade de código é reduzida e a organização e reutilização é melhorada.

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { SignInComponent } from './signin/signin.component';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { SignInComponent } from './signin/signin.component';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [ SignInComponent ],
     imports: [
         CommonModule, 
-        ReactiveFormsModule 
+        ReactiveFormsModule,
+        VMessageModule
     ]
 })
 export class HomeModule { }
