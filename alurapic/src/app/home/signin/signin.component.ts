d985cf9948c2bb37afc17893921f3ce64c0ded9e
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-08-25 11:23:49 -0300

    Enviando as credenciais do form login para a API. A estrutura Core foi criada no sistema para conter o código referente às funcionalidades de autenticação, que por agora consiste em um servie de autenticação (AuthService) que realiza uma requisição HTTP Post para a API passando o userName e a senha inseridos no formulário. O AuthService é usado pelo SignInComponent, que pega os dados do formulário através do loginForm. Se a autenticação for bem sucedida, um alerta de sucesso é exibido. Se não for, é exibido um alerta de erro, um log com o objeto de erro e o formulário é resetado através do objeto de loginForm.

--- alurapic/src/app/home/signin/signin.component.ts
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -8,12 +9,29 @@ export class SignInComponent implements OnInit {
     
     loginForm: FormGroup;
     
-    constructor(private formBuilder: FormBuilder) { }
+    constructor(
+        private authService: AuthService,
+        private formBuilder: FormBuilder
+    ) { }
     
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
         });
+    }
+
+    login() {
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+        this.authService
+            .authenticate(userName, password)
+            .subscribe(
+                () => alert('Usuário autenticado!'),
+                error => {
+                    alert('Usuário ou senha inválidos!');
+                    console.log(error);
+                    this.loginForm.reset();
+                });
     }
 }
