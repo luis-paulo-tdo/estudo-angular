f3e0efb0f46285c3f1b39ddd690fd7c68db88e86
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-08-31 07:08:18 -0300

    Implementação de método logout no UserService, que é responsável pela remoção do Token do LocalStorage da aplicação e de emitir um valor nulo para todos os Componentes que estão escutando o seu BehaviorSubject. Este método de UserService é chamado usado por um método logout do HeaderComponent, que uma vez feito isso, utiliza o Router para redirecionar o usuário de volta para a página de login. O método logout do Header foi associado ao evento de clique de um link no Template.

--- alurapic/src/app/core/header/header.component.ts
+++ alurapic/src/app/core/header/header.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 
 import { User } from '../user/user';
 import { UserService } from '../user/user.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'ap-header',
@@ -12,7 +13,14 @@ export class HeaderComponent {
 
     user$: Observable<User>;
 
-    constructor(private userService: UserService) { 
+    constructor(
+        private userService: UserService,
+        private router: Router) { 
         this.user$ = userService.getUser();
+    }
+
+    logout() {
+        this.userService.logout();
+        this.router.navigate(['']);
     }
 }
