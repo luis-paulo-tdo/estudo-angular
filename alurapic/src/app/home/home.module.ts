a37f159778ce67e77d0c3b08297bd42dfbaac3b5
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-09-01 07:52:47 -0300

    Criação do Componente de Registros de Usuário (SignUpComponent) na estrutura Home, junto ao SignInComponent. Declarado no HomeModule e tendo uma rota configurada para ele, o SignUpComponent leva consigo um formulário com o botão submit e um link que possibilita que o usuário volte para a página de login. Por padrão, o Angular tenta adicionar um controle de formulário em qualquer tag <form> que ele encontrar, exigindo que o módulo do Componente importe o FormsModule para isso. Como já havia sido utilizado o controle do ReactiveFormsModule no formulário de SignIn, não foi preciso realizar a importação do FormsModule. Mas agora que o form do SignUp não tem controle nenhum por enquanto, foi necessário realizar esta importação.

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { SignInComponent } from './signin/signin.component';
+import { SignUpComponent } from './signup/signup.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
-    declarations: [ SignInComponent ],
+    declarations: [
+        SignInComponent, 
+        SignUpComponent
+    ],
     imports: [
         CommonModule, 
+        FormsModule,
         ReactiveFormsModule,
         RouterModule,
         VMessageModule
