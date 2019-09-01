33e249572f5a078957eca10e9e4e43290d7d76a3
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-09-01 07:26:21 -0300

    Uso da diretiva routerLink na label de solicitação de login do usuário para direcioná-lo à página de login caso ele esteja acessando alguma outra página de fotos sem estar logado. O routerLink garante o redirecionamento correto da página sem a necessidade de recarregá-la do zero, como faria um href de âncora, e assim evitar problemas de performance e a consequente perda de estado da aplicação, algo que não é característico de uma Single Page Application.

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
     declarations: [HeaderComponent],
     exports: [HeaderComponent],
-    imports: [CommonModule]
+    imports: [
+        CommonModule,
+        RouterModule
+    ]
 })
-export class CoreModule {
-
-}
+export class CoreModule { }
