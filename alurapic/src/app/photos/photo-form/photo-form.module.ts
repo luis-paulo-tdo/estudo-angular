136e04b99d7ce10f38a805f603add58fba198508
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-11-05 05:52:43 -0300

    Definição do template de formulário de adição de novas fotos. O formulário é composto por um campo para seleção de imagens, um textarea para permitir a descrição da foto e um checkbox para permitir ou não comentários.

--- alurapic/src/app/photos/photo-form/photo-form.module.ts
+++ alurapic/src/app/photos/photo-form/photo-form.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { PhotoFormComponent } from './photo-form.component';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { PhotoFormComponent } from './photo-form.component';
+import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [PhotoFormComponent],
-    imports: [CommonModule]
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        VMessageModule
+    ]
 })
 export class PhotoFormModule {
 
