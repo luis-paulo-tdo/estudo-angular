0ed0cd64368f434dc10f31b05a5c56a8df58894c
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-11-16 07:36:14 -0300

    Implementação de busca da foto na API com o ID passando via parâmetro de rota. O Observable retornado pelo método findById é utilizado como atributo em PhotoDetailsComponent para ser carregado no template via AsyncPipe. Com isso, o carregamento do componente sempre aguarda Photo ser carregado antes da renderização.

--- alurapic/src/app/photos/photo-details/photo-details.component.ts
+++ alurapic/src/app/photos/photo-details/photo-details.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
+import { Observable } from 'rxjs';
+
 @Component({
-    templateUrl: 'photo-details.component.html'
+    templateUrl: 'photo-details.component.html',
+    styleUrls: ['photo-details.css']
 })
 export class PhotoDetailsComponent implements OnInit {
 
-    id: number;
+    photo$: Observable<Photo>;
     
-    constructor(private activatedRoute: ActivatedRoute) { }
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private photoService: PhotoService
+    ) { }
     
     ngOnInit(): void {
-        this.id = this.activatedRoute.snapshot.params.photoId;
+        this.photo$ = this.photoService.findById(this.activatedRoute.snapshot.params.photoId);
     }
 }
