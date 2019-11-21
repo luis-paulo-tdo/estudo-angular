90acd5d01e4ed6f8caee037870c3d232ac766744
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-11-21 06:15:01 -0300

    Resolução do problema de refresh na lista de comentários pós-publicação por meio da utilização do operador switchMap do RxJs para, logo após a execução do serviço de adição de comentários, executar o serviço de carregamento de comentários na mesma thread que o primeiro serviço. Como o operador switchMap retorna sempre o resultado do último serviço requisitado, o seu retorno é o Observable que o atributo comments$ precisa para recarregar o template com os comentários obtidos por meio do AsyncPipe. O operador do RxJS tap também foi utilizado, permitindo a execução de um código arbitrário antes que o resultado do último serviço seja retornado, código este para resetar o formulário de comentários após a sua publicação.

--- alurapic/src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ alurapic/src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 
 import { PhotoComment } from '../../photo/photo-comment';
 import { PhotoService } from '../../photo/photo.service';
@@ -29,11 +30,11 @@ export class PhotoCommentsComponent implements OnInit {
 
     save() {
         const comment = this.commentForm.get('comment').value as string;
-        this.photoService
+        this.comments$ = this.photoService
             .addComment(this.photoId, comment)
-            .subscribe(() => {
+            .pipe(switchMap(() => this.photoService.getComments(this.photoId)))
+            .pipe(tap(() => {
                 this.commentForm.reset();
-                alert('Comentário adicionado com sucesso');
-            });
+            }));
     }
 }
