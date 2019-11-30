1122f220492882ac55eaa4cb2015d73b961dad70
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-11-30 06:14:43 -0300

    Implementando mecanismo que evita que usuários acessem rotas de fotos que já foram apagadas ou não existem nos registros da API. Foi criada uma nova rota 'not-found' para exibir o NotFoundComponent de forma navegável, enquanto as rotas inválidas '**' que antes eram as responsáveis pela exibição deste componente, agora redirecionam o usuário para a nova rota criada. Desta forma, é possível utilizar o serviço de Router para redirecionar os usuários para a exibição de NotFound. Mesmo que o observable da foto obtida em PhotoDetails esteja sendo consumido pelo AsyncPipe no template, não há problemas em se inscrever nele apenas para fazer o tratamento de erros, o que possibilita a realização do redirecionamento.

--- alurapic/src/app/photos/photo-details/photo-details.component.ts
+++ alurapic/src/app/photos/photo-details/photo-details.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
+import { UserService } from 'src/app/core/user/user.service';
 
 @Component({
     templateUrl: 'photo-details.component.html'
@@ -18,12 +19,17 @@ export class PhotoDetailsComponent implements OnInit {
         private activatedRoute: ActivatedRoute,
         private alertService: AlertService,
         private photoService: PhotoService,
-        private router: Router
+        private router: Router,
+        private userService: UserService
     ) { }
     
     ngOnInit(): void {
         this.photoId = this.activatedRoute.snapshot.params.photoId;
         this.photo$ = this.photoService.findById(this.photoId);
+        this.photo$.subscribe(() => { }, err => {
+            console.log(err);
+            this.router.navigate(['/not-found']);
+        });
     }
 
     remove() {
@@ -31,11 +37,11 @@ export class PhotoDetailsComponent implements OnInit {
             .removePhoto(this.photoId)
             .subscribe(
                 () => {
-                    this.alertService.success('Photo removed');
-                    this.router.navigate(['']);
+                    this.alertService.success('Photo removed', true);
+                    this.router.navigate(['/user', this.userService.getUserName()]);
                 },
                 error => {
-                    this.alertService.warning('Photo could not be removed');
+                    this.alertService.warning('Photo could not be removed', true);
                 }
             );
     }
