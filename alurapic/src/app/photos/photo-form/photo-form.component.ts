319b6a97331a6a3d3a978104e1c213db332c6c67
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-11-30 06:09:19 -0300

    Implementação de mecanismo que permite que o usuário decida se os alertas emitidos pela aplicação continuarão sendo exibidos após um evento de navegação de rota. Esta informação pode ser emitida junto com a mensagem que o alerta irá exibir, e se não for, será false por padrão. A finalidade dela é para que, uma vez que haja alguma outra navegação além do redirecionamento inicial em rota após uma remoção ou adição de foto, apague todos os alertas que estão sendo exibidos. Para fazer isto, o AlertService deve se inscrever no serviço de roteamento da aplicação (Router) e escutar apenas os eventos de início de nova navegação (NavigationStart).

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
+import { AlertService } from 'src/app/shared/components/alert/alert.service';
+import { UserService } from 'src/app/core/user/user.service';
 
 @Component({
   selector: 'ap-photo-form',
@@ -15,9 +17,11 @@ export class PhotoFormComponent implements OnInit {
   preview: string;
 
   constructor(
+    private alertService: AlertService,
     private formBuilder: FormBuilder,
     private photoService: PhotoService,
-    private router: Router
+    private router: Router,
+    private userService: UserService
   ) { }
 
   ngOnInit() {
@@ -34,7 +38,10 @@ export class PhotoFormComponent implements OnInit {
     
     this.photoService
       .upload(description, allowComments, this.file)
-      .subscribe(() => this.router.navigate(['']));
+      .subscribe(() => {
+        this.alertService.success('Upload complete', true);
+        this.router.navigate(['/user', this.userService.getUserName()]);
+      });
   }
 
   handleFile(file: File) {
--- alurapic/src/app/shared/components/alert/alert.service.ts
+++ alurapic/src/app/shared/components/alert/alert.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 import { Subject } from 'rxjs';
 import { Alert, AlertType } from './alert';
 
@@ -8,28 +9,46 @@ import { Alert, AlertType } from './alert';
 export class AlertService {
 
     alertSubject: Subject<Alert> = new Subject<Alert>();
+    keepAfterRouteChange = false;
 
-    success(message: string) {
-        this.alert(AlertType.SUCCESS, message);
+    constructor(private router: Router) {
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationStart) {
+                if (this.keepAfterRouteChange) {
+                    this.keepAfterRouteChange = false;
+                } else {
+                    this.clear();
+                }
+            }
+        });
     }
 
-    warning(message: string) {
-        this.alert(AlertType.WARNING, message);
+    success(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.SUCCESS, message, keepAfterRouteChange);
     }
 
-    danger(message: string) {
-        this.alert(AlertType.DANGER, message);
+    warning(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.WARNING, message, keepAfterRouteChange);
     }
 
-    info(message: string) {
-        this.alert(AlertType.INFO, message);
+    danger(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.DANGER, message, keepAfterRouteChange);
     }
 
-    private alert(alertType: AlertType, message: string) {
+    info(message: string, keepAfterRouteChange: boolean = false) {
+        this.alert(AlertType.INFO, message, keepAfterRouteChange);
+    }
+
+    private alert(alertType: AlertType, message: string, keepAfterRouteChange: boolean) {
+        this.keepAfterRouteChange = keepAfterRouteChange;
         this.alertSubject.next(new Alert(alertType, message));
     }
 
     getAlert() {
         return this.alertSubject.asObservable();
+    }
+
+    clear() {
+        this.alertSubject.next(null);
     }
 }
