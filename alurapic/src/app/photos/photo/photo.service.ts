28ca06e0aec0b96c5146fbf45cee4b09ece758d7
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-12-04 06:10:33 -0300

    Implementação do método like() no PhotoService para processar a curtida de uma foto. Se a operação for bem sucedida, o método utilizará um pipe para fazer uma operação de map() que retornará true para o Componente que fez a requisição. Se houver erro, um outro pipe vai ser usado para executar o método catchError(), onde o cabeçalho da resposta é obtido para avaliar o seu código. Se o código for 304, é retornado um Observable com o valor false utilizando o operador of(). Se for qualquer outro código, o operador throwError é usado para repassar o erro que deve ter um tratamento específico para o Componente. Para ser possível a obtenção do cabeçalho da resposta em  uma requisição, é necessário passar o parâmetro { observe: 'response' } como parâmetro ao HttpClient.

--- alurapic/src/app/photos/photo/photo.service.ts
+++ alurapic/src/app/photos/photo/photo.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 import { Photo } from "./photo";
 import { PhotoComment } from './photo-comment';
@@ -47,5 +49,14 @@ export class PhotoService {
 
     removePhoto(photoId: number) {
         return this.http.delete(API + '/photos/' + photoId);
+    }
+
+    like(photoId: number) {
+        return this.http
+            .post(API + '/photos/' + photoId + '/like', {}, { observe: 'response' })
+            .pipe(map(response => true))
+            .pipe(catchError(error => {
+                return error.status == '304' ? of(false) : throwError(error);
+            }));
     }
 }
