efe6e968638339e4050d8845db757c76776589c3
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-12-07 08:00:19 -0300

    Adequando o serviço de upload de fotos para observar os eventos ocorridos durante o processamento da requisição e o progresso do envio do arquivo/binário da foto.

--- alurapic/src/app/photos/photo/photo.service.ts
+++ alurapic/src/app/photos/photo/photo.service.ts
@@ -33,7 +33,12 @@ export class PhotoService {
         formData.append('allowComments', allowComments ? 'true' : 'false');
         formData.append('imageFile', file);
         
-        return this.http.post(API + '/photos/upload', formData);
+        return this.http.post(
+            API + '/photos/upload',
+            formData,
+            {
+                reportProgress: true
+            });
     }
 
     findById(photoId: number) {
