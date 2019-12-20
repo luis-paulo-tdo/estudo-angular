f0e2d4a0ab4c017b678baaecd397deb84b867449
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-12-20 05:49:40 -0300

    Obtenção da mensagem de erro, da rota atual em que o usuário está e do nome do usuário para compilar o JSON universal que fornecerá as informações sobre o erro lançado no front end para os servidores com os quais ele se comunica. Foi utilizado o artefato Injector para realizar uma injeção de serviços diretamente no método handlerError, garantindo que o GlobalErrorHandler funcione mesmo se houver algum erro nestes serviços injetados. Foram injetados os serviços de LocationStrategy e UserService, de onde é retirado o endereço atual em que o usuário está assim como o nome do usuário logado.

--- alurapic/src/app/errors/global-error-handler/global-error-handler.ts
+++ alurapic/src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,18 +1,28 @@
-import { ErrorHandler } from '@angular/core';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import * as StackTrace from 'stacktrace-js';
+import { UserService } from 'src/app/core/user/user.service';
 
+@Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
+
+    constructor(private injector: Injector) { }
 
     handleError(error: any): void {
 
-        console.log('Erro Global');
+        console.log('========== Erro Global ==========');
+
+        const location = this.injector.get(LocationStrategy);
+        const userService = this.injector.get(UserService);
+
+        const url = location instanceof PathLocationStrategy ? location.path() : '';
+        const message = error.message ? error.message : error.toString();
 
         StackTrace
             .fromError(error)
             .then(stackFrames => {
                 const stack = stackFrames.map(stackFrame => stackFrame.toString()).join('\n');
-                console.log('Error: ', error);
-                console.log('Stack: ', stack);
+                console.log({message, stack, url, user: userService.getUserName()});
             });
     }
 }
