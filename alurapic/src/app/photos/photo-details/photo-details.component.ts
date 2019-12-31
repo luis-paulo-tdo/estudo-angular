6a2fdded02f581a98b021c955fa6022f63d33a1b
luis.paulo.tdo <luis.paulo.tdo@gmail.com>
2019-12-31 05:24:59 -0300

    Configurando o método navigate para remover a url da foto removida da history API. Com isso, o usuário não conseguirá acessar esta url inválida através do botão 'Voltar'.

--- alurapic/src/app/photos/photo-details/photo-details.component.ts
+++ alurapic/src/app/photos/photo-details/photo-details.component.ts
@@ -38,7 +38,7 @@ export class PhotoDetailsComponent implements OnInit {
             .subscribe(
                 () => {
                     this.alertService.success('Photo removed', true);
-                    this.router.navigate(['/user', this.userService.getUserName()]);
+                    this.router.navigate(['/user', this.userService.getUserName()], { replaceUrl: true });
                 },
                 error => {
                     this.alertService.warning('Photo could not be removed', true);
